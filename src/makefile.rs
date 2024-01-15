9dc3058aee7533aefb11f77fc54e9f5661583aad
Alan Rempel <alan.rempel@gmail.com>
2024-01-15 14:26:01 -0500

    Implement basic makefile rule parser

--- src/makefile.rs
+++ src/makefile.rs
@@ -1,23 +1,42 @@
-use nom::{bytes::complete::tag, multi::separated_list0, IResult};
+use eyre::Report;
+use nom::bytes::complete::{is_not, tag};
+use nom::combinator::{all_consuming, map};
+use nom::error::Error;
+use nom::multi::{many0, many0_count, separated_list0};
+use nom::sequence::preceded;
+use nom::{Finish, IResult};
 
 type Thing = Vec<u8>;
 
 pub struct Rule {
     targets: Vec<Thing>,
-    prequisites: Vec<Thing>,
+    prerequisites: Vec<Thing>,
     commands: Vec<Thing>,
 }
 
-fn path(input: &str) -> IResult<&str, Thing> {
-    todo!()
-}
-
-fn rule(input: &str) -> IResult<&str, Rule> {
+fn rule(input: &[u8]) -> IResult<&[u8], Rule> {
+    let path = map(is_not([0x20]), Vec::from);
     let (input, targets) = separated_list0(tag(" "), path)(input)?;
+    let (input, _) = many0_count(tag(" "))(input)?;
+    let (input, _) = tag(":")(input)?;
+    let (input, _) = many0_count(tag(" "))(input)?;
+    let (input, prerequisites) = separated_list0(tag(" "), path)(input)?;
+    let (input, commands) = many0(preceded(tag("\n\t"), path))(input)?;
+    let (input, _) = many0_count(tag("\n"))(input)?;
     let rule = Rule {
         targets,
-        prequisites: todo!(),
-        commands: todo!(),
+        prerequisites,
+        commands,
     };
     Ok((input, rule))
+}
+
+pub fn parse(input: &[u8]) -> Result<Vec<Rule>, Report> {
+    Ok(all_consuming(many0(rule))(input)
+        .finish()
+        .map(|(_, rules)| rules)
+        .map_err(|Error { input, code }| Error {
+            input: String::from_utf8_lossy(input),
+            code,
+        })?)
 }
